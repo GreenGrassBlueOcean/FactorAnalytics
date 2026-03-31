8239f743d77a222a09bd9dacac3c96459e88fa90
Laurens <43173895+GreenGrassBlueOcean@users.noreply.github.com>
2026-03-31 22:01:21 +0200

    refactor: remove dead zScore() function from fitFfm.R
    Never called — z-score standardization is handled by standardizeExposures()
    in fitFfmDT.R. Also had a latent grepl() argument-swap bug (pattern and
    string reversed) that would have caused incorrect matching on partial inputs.

--- R/fitFfm.R
+++ R/fitFfm.R
@@ -346,40 +346,6 @@ fitFfm <- function(data, asset.var, ret.var, date.var, exposure.vars,
 }
 
 
-### function to calculate z-scores for numeric exposure i using weights w
-## x is a data.frame object, i is a character string and w has same length as x
-# rob.stats is a logical argument to compute robust location and scale
-
-zScore <- function(x, i, w, rob.stats, z.score, asset.names) {
-  if (grepl(z.score, "crossSection")) {
-    if (rob.stats) {
-      x_bar <- median(w * x[[i]])
-      (x[[i]] - x_bar)/mad(x[[i]], center = x_bar)
-    } else {
-      x_bar <- mean(w * x[[i]])
-      n <- length(x[[i]])
-      # use equal weighted squared deviation about the weighted mean
-      (x[[i]] - x_bar)/sqrt(sum((x[[i]] - x_bar) ^ 2)/(n - 1))
-    }
-  } else {
-    N <- length(asset.names)
-    exposures <- matrix(w * x[[i]], nrow = N)
-    sigmaEWMA <- stdExpo <- exposures
-    meanExp <- apply(exposures, 1, mean)
-    sigmaExp <- apply(exposures, 1, sd)
-
-    for (j in 1:N) {
-      ts <- (exposures[j, ] - meanExp[j])^2
-      var_past_2 <- sigmaExp[j] ^ 2
-      sigmaEWMA[j, ] <- sapply(ts, function(x) var_past_2 <<- 0.10 * x + 0.90 * var_past_2)
-      if (any(sigmaEWMA[j, ] == 0)) {
-        sigmaEWMA[j, ] <- 1
-      }
-    }
-    as.vector((exposures -  meanExp) / sqrt(sigmaEWMA))
-  }
-}
-
 
 #' @param object a fit object of class \code{ffm} which is returned by
 #' \code{fitFfm}
